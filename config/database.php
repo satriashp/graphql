097cbff7111710eb5c5ef9e743d4755b0b711328
Satria Sahputra <satria.unp@gmail.com>
2019-02-08 17:20:25 +0700

    update database config

--- config/database.php
+++ config/database.php
@@ -1,4 +1,15 @@
 <?php
+$DATABASE_URL = [
+    "host" => null,
+    "port" => null,
+    "path" => null,
+    "user" => null,
+    "pass" => null
+];
+
+if (env('APP_ENV', 'production') === 'production') {
+    $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
+}
 
 return [
 
@@ -58,16 +69,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
